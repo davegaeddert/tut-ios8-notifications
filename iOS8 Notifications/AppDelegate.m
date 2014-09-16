b1f7d274c8122e1809296da1a6fea235fe39d8fe
Dave Gaeddert <dave.gaeddert@gmail.com>
2014-09-16 17:15:23 -0500

    added local notification example

--- iOS8 Notifications/AppDelegate.m
+++ iOS8 Notifications/AppDelegate.m
@@ -17,29 +17,96 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // First, create an action
+    UIMutableUserNotificationAction *acceptAction = [self createAction];
+    
+    // Second, create a category and tie those actions to it (only the one action for now)
+    UIMutableUserNotificationCategory *inviteCategory = [self createCategory:@[acceptAction]];
+    
+    // Third, register those settings with our new notification category
+    [self registerSettings:inviteCategory];
+    
+    // Now send ourselves a local notification
+    [self sendLocalNotification];
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (UIMutableUserNotificationAction *)createAction {
+    UIMutableUserNotificationAction *acceptAction = [[UIMutableUserNotificationAction alloc] init];
+    acceptAction.identifier = @"ACCEPT_IDENTIFIER";
+    acceptAction.title = @"Accept";
+    
+    // Given seconds, not minutes, to run in the background
+    acceptAction.activationMode = UIUserNotificationActivationModeBackground;
+    
+    // If YES the actions is red
+    acceptAction.destructive = NO;
+    
+    // If YES requires passcode, but does not unlock the device
+    acceptAction.authenticationRequired = NO;
+    
+    return acceptAction;
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (UIMutableUserNotificationCategory *)createCategory:(NSArray *)actions {
+    UIMutableUserNotificationCategory *inviteCategory = [[UIMutableUserNotificationCategory alloc] init];
+    inviteCategory.identifier = @"INVITE_CATEGORY";
+    
+    // You can define up to 4 actions in the 'default' context
+    // On the lock screen, only the first two will be shown
+    // If you want to specify which two actions get used on the lockscreen, use UIUserNotificationActionContextMinimal
+    [inviteCategory setActions:actions forContext:UIUserNotificationActionContextDefault];
+    
+    // These would get set on the lock screen specifically
+    // [inviteCategory setActions:@[declineAction, acceptAction] forContext:UIUserNotificationActionContextMinimal];
+    
+    return inviteCategory;
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+- (void)registerSettings:(UIMutableUserNotificationCategory *)category {
+    UIUserNotificationType types = (UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound);
+    
+    NSSet *categories = [NSSet setWithObjects:category, nil];
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:categories];
+    
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+- (void)sendLocalNotification {
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.alertBody = @"Hey!";
+    notification.category = @"INVITE_CATEGORY";
+    
+    // The notification will arrive in 5 seconds, leave the app or lock your device to see
+    // it since we aren't doing anything to handle notifications that arrive while the app is open
+    notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:5];
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+    
+    // Get the notifications types that have been allowed, do whatever with them
+    UIUserNotificationType allowedTypes = [notificationSettings types];
+    
+    NSLog(@"Registered for notification types: %u", allowedTypes);
+    
+    // You can get this setting anywhere in your app by using this:
+    // UIUserNotificationSettings *currentSettings = [[UIApplication sharedApplication] currentUserNotificationSettings];
 }
+
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forLocalNotification:(UILocalNotification *)notification completionHandler:(void (^)())completionHandler {
+    
+    if ([identifier isEqualToString:@"ACCEPT_IDENTIFIER"]) {
+        // handle it
+        NSLog(@"Invite accepted! Handle that somehow...");
+    }
+    
+    // Call this when you're finished
+    completionHandler();
+}
+
 
 @end
